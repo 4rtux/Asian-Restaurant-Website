16116b13c0201a52e27709558dfd5db4a442e757
100472007 <100472007@alumnos.uc3m.es>
2023-10-16 01:42:57 +0200

    Added DNI, first name and last name validation

--- scripts.js
+++ scripts.js
@@ -1,11 +1,21 @@
 // script.js
+
+// Variable que cuenta el n de validaciones correctas, si es igual a 4, se guarda en localStorage
+var counter = 0;
+
 document.addEventListener('DOMContentLoaded', function () {
     const openModalDiv = document.getElementById('openModalDiv');
     const signupModal = document.getElementById('signupModal');
-    const signupForm = document.getElementById('signupForm');
+
     const dniInput = document.getElementById('dni');
+    const dniValidationMessage = document.getElementById('dniValidationMessage');
+
     const firstNameInput = document.getElementById('firstName');
+    const firstNameValidationMessage = document.getElementById('firstNameValidationMessage');
+
     const lastNameInput = document.getElementById('lastName');
+    const lastNameValidationMessage = document.getElementById('lastNameValidationMessage');
+
     const telephoneInput = document.getElementById('telephone');
     const emailInput = document.getElementById('email');
     const submitBtn = document.getElementById('submitBtn');
@@ -13,6 +23,9 @@ document.addEventListener('DOMContentLoaded', function () {
     const clearBtn = document.getElementById('clearBtn');
 
     openModalDiv.addEventListener('click', function(event) {
+        dniValidationMessage.style.display = 'none';
+        firstNameValidationMessage.style.display = 'none';
+        lastNameValidationMessage.style.display = 'none';
         signupModal.style.display = 'block';
         event.preventDefault();
     });
@@ -39,17 +52,78 @@ document.addEventListener('DOMContentLoaded', function () {
 
         // Perform validation here (e.g., DNI, telephone, email format)
 
-        // Store in localStorage
-        const userData = {
-            dni,
-            firstName,
-            lastName,
-            telephone,
-            email,
-        };
-        localStorage.setItem('userData', JSON.stringify(userData));
+        const numericPart = dni.slice(0, 8); // Get the first 8 characters (numeric part)
+        const providedLetter = dni.slice(8); // Get the provided letter
+
+        // Function to calculate the letter for a given DNI number
+        function calculateDNILetter(dniNumber) {
+            const letters = 'TRWAGMYFPDXBNJZSQVHLCKE';
+            const numericDNI = dniNumber % 23;
+            return letters.charAt(numericDNI);
+        }
+
+        if (dniInput.validity.valid) {
+            const calculatedLetter = calculateDNILetter(Number(numericPart));
+            
+            if (calculatedLetter.toUpperCase() === providedLetter.toUpperCase()) {
+
+                counter++;
+
+                // Store in localStorage
+                /*const userData = {
+                    dni,
+                    firstName,
+                    lastName,
+                    telephone,
+                    email,
+                };
+
+                localStorage.setItem('userData', JSON.stringify(userData));
+                */
+            } else {
+                dniValidationMessage.style.display = 'inline';
+                dniValidationMessage.textContent = 'DNI is not valid.';
+                dniValidationMessage.style.color = 'red';
+
+                // Remove data from localStorage
+                // localStorage.removeItem('userData');
+            }
+        } else {
+            dniValidationMessage.style.display = 'inline';
+            dniValidationMessage.textContent = 'DNI format is not valid.';
+            dniValidationMessage.style.color = 'red';
+
+            // Remove data from localStorage
+            // localStorage.removeItem('userData');
+        }
+        
+        if (firstNameInput.validity.valid) {
+            counter++;
+        } else {
+            firstNameValidationMessage.style.display = 'inline';
+            firstNameValidationMessage.textContent = 'Invalid First Name.';
+            firstNameValidationMessage.style.color = 'red';
+        }
+        if (lastNameInput.validity.valid) {
+            counter++;
+        } else {
+            lastNameValidationMessage.style.display = 'inline';
+            lastNameValidationMessage.textContent = 'Invalid Last Name.';
+            lastNameValidationMessage.style.color = 'red';
+        }
+        
+        
+        
+        
+        
+        console.log(dniValidationMessage.textContent);
+
+        // Check if the data was stored correctly
+        var usr = localStorage.getItem('userData');
+        console.log(usr);
+
 
         // Close the modal
-        signupModal.style.display = 'none';
+        // signupModal.style.display = 'none';
     });
 });
